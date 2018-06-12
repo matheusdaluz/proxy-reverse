75e7ee21f143817862a8144d2f597ebd1913b99a
Matheus da luz <matheus.costa@m2msolutions.com.br>
2018-06-12 12:33:00 -0300

    projeto inicial

---
+++ src/main/java/br/com/proxyreverse/ProxyReverseDesafioApplication.java
@@ -0,0 +1,12 @@
+package br.com.proxyreverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyReverseDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyReverseDesafioApplication.class, args);
+	}
+}
