a30a43c135bedd0c186792390557dbbc0b6fb9cd
Matheus da luz <matheus.costa@m2msolutions.com.br>
2018-06-13 15:04:04 -0300

    inclusão do logger e inclusão do ssl no contexto

--- src/main/java/br/com/proxyreverse/client/HttpsClient.java
+++ src/main/java/br/com/proxyreverse/client/HttpsClient.java
@@ -9,10 +9,15 @@ import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HttpsClient {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(HttpsClient.class);
+		
 	public static void main(String[] args) {
-
+			
 		HttpsURLConnection connection = null;
 		try {
 			URL url = new URL("https://www.google.com");
@@ -21,30 +26,30 @@ public class HttpsClient {
 			SSLSocketFactory sslSocketFactory = getFactorySimple();
 			connection.setSSLSocketFactory(sslSocketFactory);
 
-			System.out.println("HTTP Response Code " + connection.getResponseCode());
-			System.out.println("HTTP Response Message " + connection.getResponseMessage());
-			System.out.println("HTTP Content Length " + connection.getContentLength());
-			System.out.println("HTTP Content Type " + connection.getContentType());
-			System.out.println("HTTP Cipher Suite " + connection.getCipherSuite());
+			logger.debug("HTTP Response Code " + connection.getResponseCode());
+			logger.info("HTTP Response Message " + connection.getResponseMessage());
+			logger.info("HTTP Content Length " + connection.getContentLength());
+			logger.info("HTTP Content Type " + connection.getContentType());
+			logger.info("HTTP Cipher Suite " + connection.getCipherSuite());
 
 			Certificate[] serverCertificate = connection.getServerCertificates();
 
 			for (Certificate certificate : serverCertificate) {
-				System.out.println("\n");
+				logger.info("\n");
 
-				System.out.println("Certificate Type " + certificate.getType());
-				System.out.println("Cert Hash Code : " + certificate.hashCode());
-				System.out.println("Cert Public Key Algorithm : " + certificate.getPublicKey().getAlgorithm());
-				System.out.println("Cert Public Key Format : " + certificate.getPublicKey().getFormat());
+				logger.info("Certificate Type " + certificate.getType());
+				logger.info("Cert Hash Code : " + certificate.hashCode());
+				logger.info("Cert Public Key Algorithm : " + certificate.getPublicKey().getAlgorithm());
+				logger.info("Cert Public Key Format : " + certificate.getPublicKey().getFormat());
 
 				if (certificate instanceof X509Certificate) {
 					X509Certificate x509cert = (X509Certificate) certificate;
 
 					Principal principal = x509cert.getSubjectDN();
-					System.out.println("Certificate Subject DN " + principal.getName());
+					logger.info("Certificate Subject DN " + principal.getName());
 
 					principal = x509cert.getIssuerDN();
-					System.out.println("Certificate IssuerDn " + principal.getName());
+					logger.info("Certificate IssuerDn " + principal.getName());
 				}
 			}
 
@@ -54,12 +59,12 @@ public class HttpsClient {
 			if (connection != null) {
 				connection.disconnect();
 			}
-			System.out.println(e.getMessage());
+			logger.error(e.getMessage());
 		}
 	}
 
 	private static SSLSocketFactory getFactorySimple() throws Exception {
-		SSLContext context = SSLContext.getInstance("TLS");
+		SSLContext context = SSLContext.getInstance("SSL");
 		context.init(null, null, null);
 		return context.getSocketFactory();
 	}
