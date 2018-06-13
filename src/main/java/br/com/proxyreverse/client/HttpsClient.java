c4a069dcce7e4540c5799de6d3a42af324d13f85
Matheus da luz <matheus.costa@m2msolutions.com.br>
2018-06-13 14:40:16 -0300

    multiplos certificados

--- src/main/java/br/com/proxyreverse/client/HttpsClient.java
+++ src/main/java/br/com/proxyreverse/client/HttpsClient.java
@@ -4,8 +4,6 @@ import java.net.URL;
 import java.security.Principal;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -13,52 +11,50 @@ import javax.net.ssl.SSLSocketFactory;
 
 public class HttpsClient {
 
-	private static List<String> targetURL = new ArrayList<String>();
-
 	public static void main(String[] args) {
-		targetURL.add("https://www.google.com");
-		
-		
-		for (String httpURL : targetURL) {
-			HttpsURLConnection connection = null;
-			try {
-				System.out.println("Try to connect to the URL " + httpURL + " ...");
-				URL url = new URL(httpURL);
-				connection = (HttpsURLConnection) url.openConnection();
 
-				SSLSocketFactory sslSocketFactory = getFactorySimple();
-				connection.setSSLSocketFactory(sslSocketFactory);
+		HttpsURLConnection connection = null;
+		try {
+			URL url = new URL("https://www.google.com");
+			connection = (HttpsURLConnection) url.openConnection();
 
-				System.out.println("HTTP Response Code " + connection.getResponseCode());
-				System.out.println("HTTP Response Message " + connection.getResponseMessage());
-				System.out.println("HTTP Content Length " + connection.getContentLength());
-				System.out.println("HTTP Content Type " + connection.getContentType());
-				System.out.println("HTTP Cipher Suite " + connection.getCipherSuite());
+			SSLSocketFactory sslSocketFactory = getFactorySimple();
+			connection.setSSLSocketFactory(sslSocketFactory);
 
-				Certificate[] serverCertificate = connection.getServerCertificates();
+			System.out.println("HTTP Response Code " + connection.getResponseCode());
+			System.out.println("HTTP Response Message " + connection.getResponseMessage());
+			System.out.println("HTTP Content Length " + connection.getContentLength());
+			System.out.println("HTTP Content Type " + connection.getContentType());
+			System.out.println("HTTP Cipher Suite " + connection.getCipherSuite());
 
-				for (Certificate certificate : serverCertificate) {
-					System.out.println("Certificate Type " + certificate.getType());
+			Certificate[] serverCertificate = connection.getServerCertificates();
 
-					if (certificate instanceof X509Certificate) {
-						X509Certificate x509cert = (X509Certificate) certificate;
+			for (Certificate certificate : serverCertificate) {
+				System.out.println("\n");
 
-						Principal principal = x509cert.getSubjectDN();
-						System.out.println("Certificate Subject DN " + principal.getName());
+				System.out.println("Certificate Type " + certificate.getType());
+				System.out.println("Cert Hash Code : " + certificate.hashCode());
+				System.out.println("Cert Public Key Algorithm : " + certificate.getPublicKey().getAlgorithm());
+				System.out.println("Cert Public Key Format : " + certificate.getPublicKey().getFormat());
 
-						principal = x509cert.getIssuerDN();
-						System.out.println("Certificate IssuerDn " + principal.getName());
-					}
+				if (certificate instanceof X509Certificate) {
+					X509Certificate x509cert = (X509Certificate) certificate;
+
+					Principal principal = x509cert.getSubjectDN();
+					System.out.println("Certificate Subject DN " + principal.getName());
+
+					principal = x509cert.getIssuerDN();
+					System.out.println("Certificate IssuerDn " + principal.getName());
 				}
-
-				connection.disconnect();
-
-			} catch (Exception e) {
-				if (connection != null) {
-					connection.disconnect();
-				}
-				System.out.println(e.getMessage());
 			}
+
+			connection.disconnect();
+
+		} catch (Exception e) {
+			if (connection != null) {
+				connection.disconnect();
+			}
+			System.out.println(e.getMessage());
 		}
 	}
 
